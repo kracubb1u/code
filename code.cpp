6115beed8c73975a9be2dc0b7f8810c0d7c3826c
kracubb1u <Eddihoc@mail.ru>
2015-02-19 20:40:00 +0200

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,42 +1,51 @@
+package test;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import java.util.Arrays;
 
-#include <iostream>
-#include <set>
-#include <string>
+public final class EXAMPLE {
+	static class Circle implements Comparable {
+		int x;
+		long y;
+		float z;
+		
+		Circle(int x,long y, float z){
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+		
+		@Override
+		public int compareTo(Object arg0) {
+			Circle another = (Circle) arg0;
+			
+			if(another.x < x && another.y < y && another.z  < z){
+				return 1;
+			}
+			
+			if(another.x > x && another.y > y && another.z  > z){
+				return -1;
+			}
+			
+			return 0;
+		}
+		
+	}
+	public static void main(String args[]) throws IOException {
+		Circle a = new Circle(1,2L,1.00001F);
+		Circle b = new Circle(10,10000000L,13.03232323f);
+		
+		Circle mas[] = new Circle[]{b,a};
+		
+		
+		
+		System.out.println(" b == mas[0] == " + (mas[0] == b) + ", a == mas[1] " + (mas[1] == a) );
+		
+		Arrays.sort(mas);
+		
+		System.out.println(" b == mas[0] == " + (mas[0] == b) + ", a == mas[1] " + (mas[1] == a) );
+	}
 
-using namespace std;
-
-int main() 
-{
-	set<char> allNumbers;
-	set<char> input;
-
-	for(char i = '0' ; i <= '9'; i++)
-		allNumbers.insert(i);
-	
-	string str;	
-	getline(cin,str);
-	
-
-	for(int i = 0; i < str.length(); i++)
-		input.insert(str[i]);
-
-	set<char> result; 	
-						
-	for(set<char>::iterator it = allNumbers.begin(); it != allNumbers.end(); it++)
-		if(!(input.count(*it) && allNumbers.count(*it)) )
-			result.insert(*it);
-	
-	cout<<result.size()<<endl;
-	
-	set<char>::iterator eit = --result.end();
-
-	for(set<char>::iterator it = result.begin(); it != result.end(); it++)
-		if(it != eit)
-			cout<<*it<<" ";
-		else
-			cout<<*it;
-
-	cout<<endl;
-	
-	return 0;
 }
